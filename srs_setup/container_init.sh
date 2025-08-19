ccf656992d497d3bb966c9e8baf7f19310c205b9
moyiii-ai <19307130296@fudan.edu.cn>
2025-08-18 23:45:28 -0700

    fix: use specific vllm precompiled wheel location

--- srs_setup/container_init.sh
+++ srs_setup/container_init.sh
@@ -12,6 +12,9 @@ pip install --upgrade pip setuptools wheel setuptools_scm jinja2 ninja
 
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 
+export VLLM_COMMIT=6d8d0a24c02bfd84d46b3016b865a44f048ae84b 
+export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
+
 VLLM_USE_PRECOMPILED=1 uv pip install --system --editable /vllm-workspace/vllm
 
 pip install -r /vllm-workspace/vllm/srs_setup/srs_requirements.txt
